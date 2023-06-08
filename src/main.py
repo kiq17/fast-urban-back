361364d79426adca327f4a8e0f1f58a296a8e18d
Caique <caiquedecastrosilva@yahoo.com.br>
2023-06-08 14:23:48 -0300

    :sparkles: :feat add cors

--- src/main.py
+++ src/main.py
@@ -1,8 +1,20 @@
 from fastapi import FastAPI
 from .routes import usersRoute, ordersRoute, productsRoute
-
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:8000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(usersRoute.router)
 app.include_router(ordersRoute.router)
